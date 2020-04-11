8a4600acad8220b175ed96f908609675751fc71f
Habib Afolabi <habibwritescode@gmail.com>
2020-04-11 20:13:56 +0100

    Add server-side code

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
+/*Dependencies*/
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,47 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+
+const server = app.listen(port, listening);
+// Callback to debug
+function listening() {
+    console.log('server is running');
+    console.log(`running on localhost: ${port}`);
+}
+
+// Initialize all route with a callback function
+app.get('/all', getData);
+// Callback function to complete GET '/all'
+function getData(req, res) {
+    res.send(projectData);
+}
+
+
+// Post Route
+app.post('/add', addData);
+
+function addData(req, res) {
+    newEntry = {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        response: req.body.response
+    }
+    projectData.push(newEntry)
+    // res.send(projectData);
+}
+
+// // UI update
+// app.get('/all', getAllData);
+
+// function getAllData(req,res){
+//     res.send(projectData);
+// }
